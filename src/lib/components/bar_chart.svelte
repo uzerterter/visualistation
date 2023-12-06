1efe68411759a386d734a35e0032d2f388c9aabb
a <a@example.com>
2023-12-06 20:45:09 +0100

    11-barchart: formatting

--- src/lib/components/bar_chart.svelte
+++ src/lib/components/bar_chart.svelte
@@ -6,7 +6,7 @@
 	let stats = null;
 
 	let svgLocal;
-    let tooltip;
+	let tooltip;
 
 	// Linienfernverkehr mit Eisenbahnen
 	// Linienfernverkehr mit Omnibussen
@@ -64,7 +64,7 @@
 		//const maxVal = d.reduce((agg, v) => Math.max(agg, v["Liniennahverkehr insgesamt"] ?? 0), 0)
 		let maxVal = 0;
 		for (const x of d) {
-            if(!ccr.map(c=>c.orig).includes(x.Art)) continue
+			if (!ccr.map((c) => c.orig).includes(x.Art)) continue;
 			//if (x.Art !== "Liniennahverkehr insgesamt") continue
 			maxVal = Math.max(x[selected.orig], maxVal);
 		}
@@ -107,16 +107,16 @@
 		const axisYears = d3.axisBottom(scaleYears).tickSizeOuter(0); // Remove outer ticks
 		const axisValues = d3.axisLeft(scaleValues).tickSizeOuter(0); // Remove outer ticks
 
-        // mouse move in between bars to avoid jumpy behavior (optional)
-        svg.append("g")
-            .append("rect")
-            .attr("width", width)
-            .attr("height", height)
-            .style("opacity", 0)
-            .on('mousemove', function(event) {
-                    tt.style("left", `${event.pageX-ttw/2}px`)
-                      .style("top", `${event.pageY-tth-10}px`)
-            })
+		// mouse move in between bars to avoid jumpy behavior (optional)
+		svg
+			.append('g')
+			.append('rect')
+			.attr('width', width)
+			.attr('height', height)
+			.style('opacity', 0)
+			.on('mousemove', function (event) {
+				tt.style('left', `${event.pageX - ttw / 2}px`).style('top', `${event.pageY - tth - 10}px`);
+			});
 
 		svg
 			.append('g')
@@ -165,8 +165,8 @@
 			return d[selected.orig];
 		}
 
-        const tt = d3.select(tooltip)
-        let [ttw, tth] = [null, null]
+		const tt = d3.select(tooltip);
+		let [ttw, tth] = [null, null];
 
 		for (const [i, c] of ccr.entries()) {
 			const g = svg.append('g');
@@ -181,21 +181,23 @@
 				.attr('class', 'bar')
 				.attr('fill', colors[c.color])
 				.attr('transform', `translate(50, 0)`)
-                .on('mouseover', function(_, x) {
-                    const color = colors[ccr.filter(c=>c.orig===x.Art)[0].color]
-                    tt.transition().duration(0).style("opacity", 1).style("color", color)                    
-                    tt.html(d3.format(",")(x[selected.orig]))
-                    const rect = tt.node().getBoundingClientRect()
-                    ttw = rect.width
-                    tth = rect.height
-                })
-                .on('mousemove', function(event) {
-                    tt.style("left", `${event.pageX-ttw/2}px`)
-                      .style("top", `${event.pageY-tth-10}px`)
-                })
-                .on('mouseout', function() {
-                    tt.transition().duration(100).style("opacity", 0)
-                })
+				.on('mouseover', function (_, x) {
+					const color = colors[ccr.filter((c) => c.orig === x.Art)[0].color];
+					tt.transition().duration(0).style('opacity', 1).style('color', color);
+					tt.html(d3.format(',')(x[selected.orig]));
+					const rect = tt.node().getBoundingClientRect();
+					ttw = rect.width;
+					tth = rect.height;
+				})
+				.on('mousemove', function (event) {
+					tt.style('left', `${event.pageX - ttw / 2}px`).style(
+						'top',
+						`${event.pageY - tth - 10}px`
+					);
+				})
+				.on('mouseout', function () {
+					tt.transition().duration(100).style('opacity', 0);
+				})
 				.attr('x', function (d) {
 					const x = scaleYears(d.Jahr);
 					const w = scaleYears.bandwidth();
@@ -219,7 +221,7 @@
 	});
 </script>
 
-<div bind:this={tooltip} class="tooltip"></div>
+<div bind:this={tooltip} class="tooltip" />
 
 <div id="barchart-toprow">
 	<div id="barchart-dropdown">
@@ -263,15 +265,15 @@
 </div>
 
 <style>
-    .tooltip {
-        position: absolute;
-        opacity: 0;
-        pointer-events: none;
-        background-color: #fff;
-        border: 1px solid #000;
-        padding: 10px;
-    }
-    
+	.tooltip {
+		position: absolute;
+		opacity: 0;
+		pointer-events: none;
+		background-color: #fff;
+		border: 1px solid #000;
+		padding: 10px;
+	}
+
 	#bar-chart {
 		width: 100%;
 		height: 100%;
