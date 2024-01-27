5f8fefb8790b7b502162f2a858ec8f536b59b0ed
Linus Stetter <linus.stetter@campus.lmu.de>
2024-01-27 14:00:39 +0100

    bugfix state highlighting left

--- src/lib/components/doughnut_chart_In.svelte
+++ src/lib/components/doughnut_chart_In.svelte
@@ -85,7 +85,7 @@
 
 	function handleResize() {
 		if (typeof window !== 'undefined') {
-			createDoughnutChart(data, currentYear);
+			createDoughnutChart(data, currentYear, highlightedState);
 		}
 	}
 
@@ -215,7 +215,7 @@
 	onMount(() => {
 		ready = true;
 		if (typeof window !== 'undefined') {
-			createDoughnutChart(data, currentYear);
+			createDoughnutChart(data, currentYear, highlightedState);
 		}
 
 		window.addEventListener('resize', handleResize);
